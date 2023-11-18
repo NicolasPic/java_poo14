5e74eb91d5780b0b7499006a070900ef94e09a87
Nicolas Piccoli <nicolaslas-piccoli@hotmail.com>
2023-11-18 10:23:53 -0300

    Add objto

--- src/programa/Main.java
+++ src/programa/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
+import entidades.Votos;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -22,13 +24,13 @@ public class Main {
 				String[] campos = linha.split(",");
 				String nome = campos[0];
 				int cont = Integer.parseInt(campos[1]);
-				
-				if (lista.containsKey(nome)) {
+				Votos voto = new Votos(nome,cont);
+				if (lista.containsKey(voto.getNome())) {
 					
-					lista.put(nome, cont + lista.get(nome));
+					lista.put(voto.getNome(), cont + lista.get(voto.getNome()));
 				}
 				else {
-					lista.put(nome, cont);
+					lista.put(voto.getNome(),voto.getVoto());
 				}
 				
 				linha = br.readLine();
